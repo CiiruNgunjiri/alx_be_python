d80c27a2f64b4476de58ad74ddb7852124f6fca7
Linda <ciiru.ngunjiri@gmail.com>
2024-09-13 23:28:09 +0300

    added python script file

---
+++ basic_operations.py
@@ -0,0 +1,31 @@
+#perform basic arithmetic exercises using python
+#assign specific values to two variables, number1 and number2
+#assign variable number1 the value 10
+#assign variable number2 the value 5
+
+number1 = 10
+number2 = 5
+
+#calculate the sum of number1 and number2
+
+addition = (number1 + number2)
+
+
+#calculate the difference between number1 and number2
+
+subtraction = (number1 - number2)
+
+#calculate the product of number1 and number2
+
+multiplication = (number1 * number2)
+
+#print ("Addition of 10 and 5 is 15")
+#print ("Subtraction of 10 and 5 is 5")
+#print ("Multiplication of 10 and 5 is 50")
+
+print (f"Addition of {number1} and {number2} is {addition}")
+print (f"Subtraction of {number1} and {number2} is {subtraction}")
+print (f"Multiplication of {number1} and {number2} is {multiplication}")
+
+
+
