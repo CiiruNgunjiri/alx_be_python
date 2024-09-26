5406f1d597c83a886e75607737baabf643c04382
Linda <ciiru.ngunjiri@gmail.com>
2024-09-26 11:32:52 +0300

    modified dailyreminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -16,31 +16,35 @@ match priority:
         reminder = (f"'{task}', is a high priority task.")
         if time_bound == 'yes':
             customized = 'It requires immediate attention today!'
-            print(f" Reminder: {reminder} {customized}")
+            print(f"Reminder: {reminder} {customized}")
         elif time_bound != 'yes':
-            
-            print(f"{reminder} You should do it as soon as possible!")
+            customized = 'You should do it as soon as possible!'
+            print(f"Reminder: {reminder} {customized}")
         else:
-            print(reminder)
+            print(f'Reminder: {reminder}')
 
     case "medium":
         reminder = (f"'{task}' is a medium priority task.")
-        if priority == 'medium' and time_bound == 'yes':
-            print(f'{reminder} Do it before the day is done.')
-        elif priority == 'medium' and time_bound != 'yes':
-            print(f"{reminder} Remember to finish it before it's too late")
+        if time_bound == 'yes':
+            customized = 'Do it before the day is done.'
+            print(f'Reminder: {reminder} {customized} ')
+        elif time_bound != 'yes':
+            customized = "Remember to finish it before it's too late."
+            print(f"Reminder: {reminder} {customized} ")
         else:
-            print(reminder)
+            print(f'Reminder: {reminder}')
 
     case "low":
         reminder = (f"'{task}' is a low priority task.") 
-        if priority == 'low' and time_bound == 'yes':
-            print(f'{reminder} Allocate time to finish it when you can.')
+        if time_bound == 'yes':
+            customized = 'Allocate time to finish it when you can.'
+            print(f'Reminder: {reminder} {customized} ')
         else:
-            print(reminder)
+            print(f'Reminder: {reminder}')
 
     case _:
-        reminder = print('Priority is invalid.')
+        reminder = 'Priority is invalid.'
+        print (f' Reminder: {reminder}')
 
 
 
