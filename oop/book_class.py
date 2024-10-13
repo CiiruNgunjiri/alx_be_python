dbb3e1ca93a0fd728c106d09f2f74f92c2655d56
Linda <ciiru.ngunjiri@gmail.com>
2024-10-13 13:54:47 +0300

    added book-class file to the folder

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+#define a book class that uses specific magic methods
+
+class Book:
+    
+    def __init__(self, title, author, year):
+        self.title = title #title of the book
+        self.author = author #author of the book
+        self.year = year #year of publication of the book
+
+    def __del__(self):
+        print (f"Deleting {self.title}")    
+    
+    def __str__(self):
+
+        return f"{self.title} by {self.author}, published in {self.year}" 
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+        
+    
